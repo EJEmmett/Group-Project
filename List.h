dab2799285896334f16633d55a2eec21530be264
EJEmmett <edemms12@gmail.com>
2019-04-08 22:46:01 -0400

    Added a few things
    Note: std::optional is only available in c++17.
    I moved from dev-cpp to Code::Blocks to make use of the newer compiler

---
+++ List.h
@@ -0,0 +1,88 @@
+#include "Repairs.h"
+#include <iostream>
+
+#ifndef LIST_H
+#define LIST_H
+
+template<typename T>
+class List{
+	public:
+		List();
+		void addNode(T);
+        float sumCost();
+		float sumHours();
+		void printList();
+
+	private:
+		struct node{
+			T data;
+			node* next;
+		};
+
+		node* head;
+		node* curr;
+		node* temp;
+};
+
+template<typename T>
+List<T>::List(){
+	head = NULL;
+	curr = NULL;
+	temp = NULL;
+}
+
+template<typename T>
+void List<T>::addNode(T dat){
+	node* n = new node;
+	n->next = NULL;
+	n->data = dat;
+	if(head){
+		curr = head;
+		while(curr->next)
+			curr = curr->next;
+		curr->next = n;
+	}else
+		head = n;
+}
+
+template<typename T>
+void List<T>::printList(){
+	curr = head;
+	while(curr){
+		std::cout << curr -> data << std::endl;
+		curr = curr->next;
+	}
+}
+
+template<>
+float List<Repairs*>::sumCost(){
+    float sum = 0;
+	curr = head;
+	while(curr){
+        sum += curr->data->getHours() * curr->data->getEmployee().getRate();
+		curr = curr->next;
+	}
+	return sum;
+}
+
+template<>
+float List<Repairs*>::sumHours(){
+    float sum = 0;
+	curr = head;
+	while(curr){
+        sum += curr->data->getHours();
+		curr = curr->next;
+	}
+	return sum;
+}
+
+template<>
+void List<Repairs*>::printList(){
+	curr = head;
+	while(curr){
+		std::cout << curr -> data -> print() << std::endl;
+		curr = curr->next;
+	}
+}
+
+#endif
--- Ticket.h
+++ Ticket.h
@@ -1,26 +1,64 @@
-#include "Customer.h"
 #include "Invoice.h"
+#include "Repairs.h"
 #include <ctime>
+#include <optional>
+
+#ifndef TICKET_H
+#define TICKET_H
 
 class Ticket{
 	public:
 		Ticket(Customer, std::string);
-		
-		void addRepair(Technician, int) // int hoursWorked
-		
-		std::string getStatus();
+
+		void addRepair(Employee, float, std::time_t, std::string); // int hoursWorked
+
 		void listRepairs();
-		std::time_t getCompletion();
-		Customer getClient();
+		std::string getCompletion() const;
+		Customer getClient() const;
+		std::string getStatus() const;
 		Invoice getInvoice();
-		
+
 	private:
 		std::time_t timeEntered;
-		std::time_t timeCompleted;
+		std::optional<std::time_t> timeCompleted;
 		std::string status;
-		List<Technician, int, std::time_t, std::string> repairsCompleted;
-		int workHours;
+		List<Repairs*> repairsCompleted;
 		Customer client;
 		std::string equipment;
-		Invoice invo;
+		std::optional<Invoice> invo;
 };
+
+Ticket::Ticket(Customer c, std::string e){
+	client = c;
+	equipment = e;
+	timeEntered = std::time(nullptr);
+	status = "Not Completed";
+}
+
+void Ticket::addRepair(Employee emp, float hoursWorked, std::time_t timeWorked, std::string workCompleted){
+	repairsCompleted.addNode(new Repairs(emp, hoursWorked, timeWorked, workCompleted));
+}
+
+void Ticket::listRepairs(){
+    repairsCompleted.printList();
+}
+
+std::string Ticket::getCompletion() const{
+	return timeCompleted ? ctime(&*timeCompleted) : "Not Completed";
+}
+
+Customer Ticket::getClient() const{
+	return client;
+}
+
+std::string Ticket::getStatus() const{
+	return status;
+}
+
+Invoice Ticket::getInvoice(){
+	if(!invo)
+        invo = Invoice(client, repairsCompleted.sumCost(), repairsCompleted.sumHours());
+    return *invo;
+}
+
+#endif
